5eb5232ca507bc87bcad88a16dde34fa1ce0b1fb
Amapo057 <jiseong2002@naver.com>
2025-07-23 15:17:13 +0900

    Update rcController.ino

--- rcController/rcController.ino
+++ rcController/rcController.ino
@@ -6,6 +6,13 @@ const int joiX = A0;
 const int joiY = A1;
 const int drsButton = 2;
 
+struct DataPack {
+  byte joiX;
+  byte joiY;
+  byte drsButton;
+};
+DataPack data;
+
 void setup() {
   Serial.begin(9600);
   pinMode(drsButton, INPUT);
@@ -15,5 +22,13 @@ void loop() {
   int xRead = analogRead(joiX);
   int yRead = analogRead(joiY);
   int drsRead = digitalRead(drsButton);
+  data.joiX = map(xRead, 0, 1023, 0, 255);
+  data.joiY = map(yRead, 0, 1023, 0, 255);
+  data.drsButton = drsRead;
 
+  Serial.print(data.joiX);
+  Serial.print(data.joiY);
+  Serial.print(data.drsButton);
+
+  delay(100);
 }
